9e618a6d54cf9824afc80f61b4934fc2e1785861
Nick Gumbs <36005687+nickgumbs@users.noreply.github.com>
2024-12-22 23:32:59 -0500

    Update ci.yaml

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,11 @@ module.exports = defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  reporter: [['list'], ['@estruyf/github-actions-reporter']],
+  reporter: [
+    ['list'],
+    ['html', { outputFolder: 'reports/playwright', open: 'never' }],
+    ['@estruyf/github-actions-reporter']
+  ],
   use: {
     baseURL: 'https://gumbs.website',
     trace: 'on-first-retry'
@@ -18,11 +22,6 @@ module.exports = defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] }
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-      testIgnore: [/.*axe.pw.js/, /lighthouse.pw.js/]
     }
   ]
 });
