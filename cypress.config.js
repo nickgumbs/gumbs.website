54731696606a0e717d1a7818a662a63d87bdf894
Nick Gumbs <36005687+nickgumbs@users.noreply.github.com>
2024-12-26 21:11:56 -0500

    Add Cypress Reports to CI on Failure

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   retries: 2,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'reports/cypress',
+    overwrite: true,
+    html: false,
+    json: true
+  },
   e2e: {
     baseUrl: 'http://localhost:8080/'
   }
